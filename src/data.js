581a70b3ef7eb8e86148bdb0be2a8356bd1bd15d
Trần Quốc Bảo <tqucbo.peter1209@yahoo.com>
2022-03-14 17:37:59 +0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        id : 1,
+        name : 'Valhein',
+        age : 20,
+        image : 'https://lienquan.garena.vn/files/champion/icon/4b36c6e5e2d1ce9dd9e2841d2902043c5ee04efeb2f2d.jpg',
+    },
+    {
+        id : 2,
+        name : 'Thane',
+        age : 38,
+        image : 'https://lienquan.garena.vn/files/champion/icon/71e488144b7dc9f13d40321ce0556efc5847d39f2071a.png',
+    },
+    {
+        id : 3,
+        name : 'Veera',
+        age : 27,
+        image : 'https://lienquan.garena.vn/files/champion/icon/07bacf5da3930066d2d6255859ca70cc5cd3d3bbe5816.jpg',
+    },
+];
